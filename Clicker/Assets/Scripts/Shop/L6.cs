79f28cf6e8a6adbffb324417c68733f67b34a4b8
55Yan66 <110537428+55Yan66@users.noreply.github.com>
2024-01-25 21:16:51 +0300

    shop_yan

---
+++ Clicker/Assets/Scripts/Shop/L6.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L6 : MonoBehaviour
+{
+    [SerializeField] private Transform popupInventoty;
+    [SerializeField] private Transform popupShop;
+    private bool isVisible = true;
+    public void ShowPopup(Transform popup)
+    {
+        isVisible = !isVisible;
+        popup.gameObject.SetActive(isVisible);
+        popup.SetAsLastSibling();
+        
+
+
+    }
+
+
+
+
+
+}
